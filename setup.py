2eb3c65157241872ed25918edf76e9b7c450b3c0
Keji Li <mail@keji.li>
2018-08-09 14:50:58 -0400

    fixed setup.py syntax error

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='algorithm',
     version='0.1',
     install_requires=['numpy', 'scipy', 'numba', 'noformat'],
-    dependency_links=['git+ssh://git@github.com:Palpatineli/noformat.git']
     packages=['algorithm'],
     extras_require={'test': ["pytest"]}
 )
